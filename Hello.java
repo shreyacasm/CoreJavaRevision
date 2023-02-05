f0c4eb2c7cee98c560cf9052af54fe372db82a0e
Shreya <64486266+shreyacasm@users.noreply.github.com>
2023-02-05 20:44:31 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+// Hi welcome to java revision 2023
+// public static void main is needed to run and class is mandatory part of Java
+// To compile - javac Hello.java
+// To run - java Hello
+class Hello
+{
+    public static void main(String a[]) {
+        System.out.println("Hello Shreya");   
+    }
+}
